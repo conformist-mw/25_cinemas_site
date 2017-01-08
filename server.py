20a210b9d6c6275d99ef2d728d452355fc1340b0
conformist-mw <conformist.mw@gmail.com>
2017-01-08 12:15:20 +0200

    added routes for site and api

--- server.py
+++ server.py
@@ -1,10 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, json
+from werkzeug.contrib.cache import SimpleCache
+import requests
+from afisha import collect_movie_data, return_raw_json, get_movies_urls
 
 app = Flask(__name__)
+cache = SimpleCache()
+app.config['JSON_AS_ASCII'] = False
+afisha_url = 'http://www.afisha.ru/msk/schedule_cinema/'
+
+
+def get_cache(url):
+    page = cache.get(url)
+    if page:
+        return page
+    page = requests.get(url).text
+    cache.add(url, page, timeout=43200)
+    return page
+
 
 @app.route('/')
 def films_list():
-    return render_template('films_list.html')
+    main_page = get_cache(afisha_url)
+    links = get_movies_urls(main_page)
+    movie_list = []
+    for link in set(links):
+        movie_page = get_cache(link)
+        movie_data = collect_movie_data(movie_page)
+        movie_list.append(movie_data)
+    return render_template('films_list.html', movies=movie_list)
+
+
+@app.route('/api')
+def api_about():
+    return render_template('api_about.html')
+
+
+@app.route('/api/movies_list', methods=['GET'])
+def api_movies_list():
+    main_page = get_cache(afisha_url)
+    links = get_movies_urls(main_page)
+    movies_json = []
+    for link in links:
+        movie_page = get_cache(link)
+        movies_json.append(return_raw_json(movie_page))
+    return Response(json.dumps(movies_json, indent=4),
+                    content_type='application/json; charset=utf-8')
+
+
+@app.route('/api/movie/<int:param>/', methods=['GET'])
+def movie_api(param):
+    url = 'http://www.afisha.ru/movie/{}'.format(param)
+    page = get_cache(url)
+    json_data = return_raw_json(page)
+    return Response(json.dumps(json_data, indent=4),
+                    content_type='application/json; charset=utf-8')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
