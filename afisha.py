a5c03627f99f4c69856b3dbe89c328b31d4bd272
conformist-mw <conformist.mw@gmail.com>
2017-01-08 21:04:21 +0200

    replace durty movie_data dict with clean dict

--- afisha.py
+++ afisha.py
@@ -1,7 +1,6 @@
 import json
 import re
 from bs4 import BeautifulSoup
-regex = re.compile(r'PT(\d+)H(\d+)M')
 default_img = 'http://www.userlogos.org/files/logos/klonex/af.jpg'
 
 
@@ -16,24 +15,28 @@ def collect_movie_data(movie_page):
     soup = BeautifulSoup(movie_page, 'lxml')
     raw_data = json.loads(
         soup.find('script', {'type': 'application/ld+json'}).text)
-    movie_data = {}
     duration = raw_data.get('duration', {'name': 'Не известно'})['name']
-    movie_data['duration'] = regex.sub(lambda m: str(
+    duration = re.sub(r'PT(\d+)H(\d+)M', lambda m: str(
         int(m.group(1)) * 60 + int(m.group(2))), duration)
-    movie_data['director'] = raw_data.get(
-        'director', {'name': 'Неизвестен'})['name']
-    movie_data['title'] = raw_data['name']
-    movie_data['year'] = re.sub(
-        r'(\d{4}).*', r'\1', raw_data['datePublished'])
-    movie_data['description'] = raw_data['description']
-    movie_data['url'] = raw_data['url']
-    movie_data['genre'] = raw_data['genre']
-    movie_data['text'] = raw_data.get('text', 'Нет описания')
-    movie_data['image'] = raw_data.get('image', default_img)
-    movie_data['rate_count'] = raw_data.get(
+    director = raw_data.get('director', {'name': 'Неизвестен'})['name']
+    year = re.sub(r'(\d{4}).*', r'\1', raw_data['datePublished'])
+    rate_count = raw_data.get(
         'aggregateRating', {'ratingCount': 0})['ratingCount']
-    movie_data['rate_value'] = raw_data.get(
+    rate_value = raw_data.get(
         'aggregateRating', {'ratingValue': 0})['ratingValue']
+    movie_data = {
+            'text': raw_data.get('text', 'Нет описания'),
+            'image': raw_data.get('image', default_img),
+            'description': raw_data['description'],
+            'genre': raw_data['genre'],
+            'title': raw_data['name'],
+            'url': raw_data['url'],
+            'rate_count': rate_count,
+            'rate_value': rate_value
+            'duration': duration,
+            'director': director,
+            'year': year,
+            }
     return movie_data
 
 
